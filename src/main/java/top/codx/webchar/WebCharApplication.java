d5c07f415f38e710d682b3cd6d207a35c94a93c0
Liuwins <1976105305@qq.com>
2022-08-11 17:31:51 +0800

    Initial commit

---
+++ src/main/java/top/codx/webchar/WebCharApplication.java
@@ -0,0 +1,13 @@
+package top.codx.webchar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCharApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCharApplication.class, args);
+    }
+
+}
