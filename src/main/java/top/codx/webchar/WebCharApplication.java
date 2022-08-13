3b9c2f259824d18bc38ff4c83bcc3369a3b69fea
Liuwins <1976105305@qq.com>
2022-08-13 18:12:13 +0800

    预设文件

--- src/main/java/top/codx/webchar/WebCharApplication.java
+++ src/main/java/top/codx/webchar/WebCharApplication.java
@@ -3,6 +3,9 @@ package top.codx.webchar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 刘城豪
+ */
 @SpringBootApplication
 public class WebCharApplication {
 
